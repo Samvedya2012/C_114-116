25506abf9ca4eef49a58e1c3c609b9861287f95f
Samvedya2012 <90142729+Samvedya2012@users.noreply.github.com>
2023-06-08 15:36:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ noseX=0;
 noseY=0;
 
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/XvfgQ4VL/nothescarythinginlife.jpg');
 }
 
 function setup() {
@@ -21,9 +22,7 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
-    fill(255,0,0);
-    stroke(255,0,0);
-    circle(noseX, noseY, 20);
+    image(clown_nose,noseX,noseY,15,30);
 }
 
 function take_snapshot() {
@@ -35,8 +34,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
